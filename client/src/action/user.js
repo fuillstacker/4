215ffe49647dd8a3f9179de1409a24703d1cb9f0
fuuilstacker <mirohsnichenkopavlo802@gmail.com>
2024-05-13 21:06:35 +0300

    first commit

---
+++ client/src/action/user.js
@@ -0,0 +1,96 @@
+import axios from 'axios'
+import { setUser } from '../reducers/userReducer'
+import { API_URL } from '../config'
+import { hideLoader, showLoader } from '../reducers/appReducer'
+
+export const register = async (email, password) => {
+    return async dispatch => {
+        try {
+            dispatch(showLoader())
+            const response = await axios.post('https://cloudstoragec.com/api/register', {
+                email,
+                password
+            })
+            console.log(response.data)
+        } catch (e) {
+            alert(e.response.data.message)
+        } finally {
+            dispatch(hideLoader())
+        }
+    }
+}
+
+export const login = (email, password) => {
+    return async dispatch => {
+        try {
+            dispatch(showLoader())
+            const response = await axios.post('https://cloudstoragec.com/api/login', {
+                email,
+                password
+            })
+            dispatch(setUser(response.data.user))
+            localStorage.setItem('token', response.data.token)
+            console.log(response.data)
+        } catch (e) {
+            alert(e.response.data.message)
+        } finally {
+            dispatch(hideLoader())
+        }
+    }
+}
+
+export const auth = () => {
+    return async dispatch => {
+        try {
+            dispatch(showLoader())
+            const response = await axios.get('https://cloudstoragec.com/api/auth',
+                { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }
+            )
+
+            dispatch(setUser(response.data.user))
+            localStorage.setItem('token', response.data.token)
+            console.log(response.data)
+        } catch (e) {
+            console.log(e.response.data.msg)
+            localStorage.removeItem('token')
+        } finally {
+            dispatch(hideLoader())
+        }
+    }
+}
+
+export const uploadAvatar = (file) => {
+    return async dispatch => {
+        try {
+            dispatch(showLoader())
+            const formData = new FormData()
+            formData.append('file', file)
+            const response = await axios.post(`https://cloudstoragec.com/api/files/avatar`, formData,
+                { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }
+            )
+            dispatch(setUser(response.data))
+        } catch (e) {
+            console.log(e)
+        } finally {
+            dispatch(hideLoader())
+        }
+    }
+}
+
+export const deleteAvatar = () => {
+    return async dispatch => {
+        try {
+            dispatch(showLoader())
+            const response = await axios.delete(`https://cloudstoragec.com/api/files/avatar`,
+                { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }
+            )
+
+            dispatch(setUser(response.data))
+        } catch (e) {
+            console.log(e)
+        } finally {
+            dispatch(hideLoader())
+        }
+    }
+}
+
